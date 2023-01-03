a9f2e1232d8e986acdf50c60221aa87dcb971841
Sergio Ruiz <sergivar24@gmail.com>
2023-01-03 09:26:44 +0100

    creacion display  consola

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 
-const parteSuperior = document.getElementById("capaSuperior")
-const parteInferior = document.getElementById("capaInferior")
+/* Listener de click */
+const pulsar = document.getElementById("btnEncendido");
+pulsar.addEventListener("click", intervalo);
 
+
+
+/* Obtiene posición de los layout */
+const parteSuperior = document.getElementById("capaSuperior");
+const parteInferior = document.getElementById("capaInferior");
 
 function posicion(parte){
     let localizacion = parte.getBoundingClientRect();
-
     return localizacion.top
 }
 
+/* Enciende el led */
+function encender(estado){
 
+    const ledEncendido = document.getElementById("ledEncendido");   
+    
+
+    if (estado == "on"){
+    
+        let color = ledEncendido.style.background="green";
+    
+    } else {
+
+        let color = ledEncendido.style.background="white";
+    }
+}
+
+/* Contador de tiempo */
 var intervaloOk = false;
 let timer;
-function intervalo(){   
 
+function intervalo(){   
+    
     if (intervaloOk==false){
-        timer = setInterval(mov1, 5);
+
+        timer = setInterval(mov, 5);
         intervaloOk=true;
         
     }else{
+        
         clearInterval(timer);
         intervaloOk=false;
     }
 
 }
+
+/* Desplazamiento de los laout */
 var contGrupo=0;
 var estado="off";
 
-function mov1(){
+function mov(){
     
     
     if (estado=="off"){
@@ -45,23 +71,30 @@ function mov1(){
         
         else{
                 clearInterval(timer);
+                intervaloOk=false;
                 estado="on";
+                encender(estado);
+                
                 }
     } else {
 
         if (posicion(parteSuperior) != posicion(parteInferior)){
                 contGrupo++;
                 parteSuperior.style.top = contGrupo * 0.03 + "em";
-                } 
+                }
+
         else if  (contGrupo >=1){
                 contGrupo--;
                 parteSuperior.style.top = contGrupo * 0.03 + "em";
                 parteInferior.style.marginTop = contGrupo * 0.03 + "em";
                 }
+
         else{
                 clearInterval(timer);
+                intervaloOk=false;
                 estado="off";
-                }
+                encender(estado);
+            }
 
     }
 
